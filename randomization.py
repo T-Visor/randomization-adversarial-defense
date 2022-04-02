936bd488398a09a8dfcd498100efa73c36bc6dfd
T-Visor <turkimbro@yahoo.com>
2022-04-02 19:34:47 -0400

    made some progress loading a directory of images into a list

--- randomization.py
+++ randomization.py
@@ -1,23 +1,69 @@
-from PIL import Image
-from pathlib import Path
 import matplotlib.pyplot as plt
 import numpy as np
-
+import uuid
 import torch
 import torchvision.transforms as T
+from PIL import Image
+from pathlib import Path
+import argparse
+import sys
+import glob
+
+
+
+def main():
+    # Get the command-line arguments
+    arguments = parse_command_line_arguments()
+
+    # Assign values from command-line
+    source_folder = arguments.source[0]
+    destination_folder = arguments.destination[0]
+
+    # Load all images from the directory
+    images = map(str, glob.iglob(source_folder + '/**/*.png', recursive=True))
+    #images = []
+
+    #for image in glob.iglob(source_folder + '/**/*.png', recursive=True):
+    #    images.append(image)
+    #    print(image)
+
+    print(list(images))
+
+
+
+
+def parse_command_line_arguments():
+    """ 
+        Parse the arguments from the command-line.
+        If no arguments are passed, the help screen will
+        be shown and the program will be terminated.
+
+    Returns:
+        the parser with command-line arguments
+    """
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-s', '--source', nargs=1, required=True,
+                        help='Source folder containing images.')
+
+    parser.add_argument('-d', '--destination', nargs=1, required=True,
+                        help='Destination folder to save randomized images to.')
+
+    # if no arguments were passed, show the help screen
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit()
+
+    return parser.parse_args()
 
 
 
 
 def img_transform(img_in, img_out=None):
-    from PIL import Image
-    import torchvision.transforms as T
-    import numpy as np
-    import uuid
      
     margin_min = 50
     margin_max = 400
-    background = (231, 187, 0)
+    background_color = (231, 187, 0) # RGB value: yellowish-gold
     
     orig_image = Image.open(img_in)
     rngs = np.random.default_rng()
@@ -27,7 +73,7 @@ def img_transform(img_in, img_out=None):
     new_width = width + padding_margins[0] + padding_margins[1]
     new_height = height + padding_margins[2] + padding_margins[3]
     
-    new_image = Image.new(orig_image.mode, (new_width, new_height), background)
+    new_image = Image.new(orig_image.mode, (new_width, new_height), background_color)
     new_image.paste(orig_image, (padding_margins[0],padding_margins[2]))
     resized_imgs = [T.Resize(size=size)(new_image) for size in ([(1500,1500)])]
 
@@ -40,4 +86,4 @@ def img_transform(img_in, img_out=None):
 
 
 if __name__ == '__main__':
-    img_transform('Valkyrae-2.png', 'Valkyrae_randomized.png')
+    main()
