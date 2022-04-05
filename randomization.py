adb7e8266ddc32bbc5d411fcf0e2befe5019bb97
T-Visor <turkimbro@yahoo.com>
2022-04-04 20:41:19 -0400

    added some comments

--- randomization.py
+++ randomization.py
@@ -12,11 +12,13 @@ from pathlib import Path
 import os
 from functools import partial
 
-#https://stackoverflow.com/questions/22444147/python-change-part-single-directory-name-of-path
 
 
 
 def main():
+    """
+        Apply random padding to an entire dataset of images.
+    """
     # Get the command-line arguments
     arguments = parse_command_line_arguments()
 
@@ -28,21 +30,42 @@ def main():
     images_found = list(map(str, glob.iglob(source_folder + '/**/*.png', recursive=True)))
 
     # Get all the target paths
-    #target_paths = list(map(copy_tree, images_found, destination_folder_instance))
     target_paths = []
     for image in images_found:
         target_paths.append(copy_tree(image, destination_folder)) 
 
-    # Output the images with randomized padding to a new directory
+    # Output the images with randomized padding to a new directory.
     list(map(img_transform, images_found, target_paths))
 
-    print(images_found)
-    print(target_paths)
+    print('===============IMAGES FOUND===============')
+    list(map(print, images_found))
+    print('==========================================\n')
+
+    print('============TRANSFORMED IMAGES============')
+    list(map(print, target_paths))
+    print('==========================================\n')
 
 
 
-def copy_tree(source, destination_folder):
-    source_path = Path(source)
+
+
+def copy_tree(source_file, destination_folder):
+    """
+        Copy a file path while maintaining its subdirectory structure
+        and replace its top-level directory with the destination folder.
+
+        Example:
+            copy_tree("Downloads/files/example.txt", "Documents") ==> "Documents/files/example.txt"
+
+    Args:
+        source_file (string): file path to copy
+
+        destination_folder (string): the new top-level folder
+
+    Returns:
+        a new file path with its top-level folder replaced.
+    """
+    source_path = Path(source_file)
     name_to_change = source_path.parts[0] # Get the first part of the file path
     target_path = "/".join([part if part != name_to_change else destination_folder
                             for part in source_path.parts])[0:]
@@ -79,6 +102,14 @@ def parse_command_line_arguments():
 
 
 def img_transform(img_in, img_out=None):
+    """
+        Apply random padding to an image.
+
+    Args:
+        img_in (string): image to add randomized padding to
+
+        img_out (string): destination file path for the new image
+    """
      
     margin_min = 50
     margin_max = 400
@@ -103,7 +134,7 @@ def img_transform(img_in, img_out=None):
         os.makedirs(os.path.dirname(img_out), exist_ok=True)
 
     resized_imgs[0].save(img_out)
-    return resized_imgs[0]
+    #return resized_imgs[0]
 
 
 
